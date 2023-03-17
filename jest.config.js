cc9d35e609f9856b7648fd01dde89606701f23bb
Ruslan Abelkharisov <abelkharisov@gmail.com>
2023-03-17 11:01:44 +0300

    add jenkins and test with qase reporter

---
+++ jest.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    globals: {},
+    reporters: [
+        'default',
+        [
+            'jest-qase-reporter',
+            {
+                logging: true,
+                runComplete: true
+            },
+        ],
+    ],
+    roots: [
+        '<rootDir>/test',
+    ],
+    testMatch: [
+        '**/__tests__/**/*.js?(x)',
+        '**/?(*.)+(spec|test).js?(x)',
+        '**/__tests__/**/*.ts?(x)',
+        '**/?(*.)+(spec|test).ts?(x)',
+    ],
+    moduleFileExtensions: [
+        'ts',
+        'tsx',
+        'js',
+        'jsx',
+        'json',
+        'node',
+    ],
+    testEnvironment: 'node',
+};
